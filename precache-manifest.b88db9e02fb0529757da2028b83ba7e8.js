8b961f27d42e6d2eddbcf2495bee238273620153
Britt Tarien <britt.tarien@gmail.com>
2020-10-12 17:30:28 +0200

    Updates

---
+++ precache-manifest.b88db9e02fb0529757da2028b83ba7e8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21e8536f1ae39f92f00b0491e94c15eb",
+    "url": "/btarien.github.io/index.html"
+  },
+  {
+    "revision": "e8eb93e67066dfce2986",
+    "url": "/btarien.github.io/static/css/main.03fef23b.chunk.css"
+  },
+  {
+    "revision": "d39f9ba7e3ecc7ad3c82",
+    "url": "/btarien.github.io/static/js/2.9868f436.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/btarien.github.io/static/js/2.9868f436.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8eb93e67066dfce2986",
+    "url": "/btarien.github.io/static/js/main.5eb7d6c9.chunk.js"
+  },
+  {
+    "revision": "4d13c49cf2d4867d307a",
+    "url": "/btarien.github.io/static/js/runtime-main.db71220f.js"
+  },
+  {
+    "revision": "c7886f467538ee68c1d4b3440c5b560d",
+    "url": "/btarien.github.io/static/media/calculator.c7886f46.png"
+  },
+  {
+    "revision": "32f2106df99be10e6ba7e3e92ff1caca",
+    "url": "/btarien.github.io/static/media/headshot.32f2106d.jpeg"
+  },
+  {
+    "revision": "f900e9800c0896401c26807c2b789115",
+    "url": "/btarien.github.io/static/media/match-up.f900e980.png"
+  },
+  {
+    "revision": "739c1e8bb0651a0b2c97e78b6e3772ad",
+    "url": "/btarien.github.io/static/media/maze.739c1e8b.png"
+  },
+  {
+    "revision": "ef9f7e4c8efa5817374109f975639bcb",
+    "url": "/btarien.github.io/static/media/row.ef9f7e4c.png"
+  },
+  {
+    "revision": "182bed0c01e6a6b303a6bd5c18503329",
+    "url": "/btarien.github.io/static/media/to-do.182bed0c.png"
+  },
+  {
+    "revision": "2939957c499586945b61179e75857269",
+    "url": "/btarien.github.io/static/media/website-cover.2939957c.mp4"
+  },
+  {
+    "revision": "a914c4c98e3f16b5f083d339a589456f",
+    "url": "/btarien.github.io/static/media/website.a914c4c9.png"
+  }
+]);
