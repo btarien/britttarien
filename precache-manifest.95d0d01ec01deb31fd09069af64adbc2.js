b5ab4602beccfb92f9fc9038654ded8d52524335
Britt Tarien <britt.tarien@gmail.com>
2021-04-02 21:32:03 +0200

    Updates

---
+++ precache-manifest.95d0d01ec01deb31fd09069af64adbc2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "637a82d02c09d22d1b6d2eed86c33d10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fb776adc9cdf89b954d",
+    "url": "/static/css/main.4790809a.chunk.css"
+  },
+  {
+    "revision": "fb0252c6dee490e4286c",
+    "url": "/static/js/2.3a9763c9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.3a9763c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fb776adc9cdf89b954d",
+    "url": "/static/js/main.93c0932a.chunk.js"
+  },
+  {
+    "revision": "ba126735810163618457",
+    "url": "/static/js/runtime-main.3374ac37.js"
+  },
+  {
+    "revision": "c7886f467538ee68c1d4b3440c5b560d",
+    "url": "/static/media/calculator.c7886f46.png"
+  },
+  {
+    "revision": "32f2106df99be10e6ba7e3e92ff1caca",
+    "url": "/static/media/headshot.32f2106d.jpeg"
+  },
+  {
+    "revision": "f900e9800c0896401c26807c2b789115",
+    "url": "/static/media/match-up.f900e980.png"
+  },
+  {
+    "revision": "739c1e8bb0651a0b2c97e78b6e3772ad",
+    "url": "/static/media/maze.739c1e8b.png"
+  },
+  {
+    "revision": "ef9f7e4c8efa5817374109f975639bcb",
+    "url": "/static/media/row.ef9f7e4c.png"
+  },
+  {
+    "revision": "182bed0c01e6a6b303a6bd5c18503329",
+    "url": "/static/media/to-do.182bed0c.png"
+  },
+  {
+    "revision": "2939957c499586945b61179e75857269",
+    "url": "/static/media/website-cover.2939957c.mp4"
+  },
+  {
+    "revision": "a914c4c98e3f16b5f083d339a589456f",
+    "url": "/static/media/website.a914c4c9.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/btarien.github.io/precache-manifest.1023cda8fa54506dff300ca8df7718fc.js"
+  "/precache-manifest.95d0d01ec01deb31fd09069af64adbc2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/btarien.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
